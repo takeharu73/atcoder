744183ed6dc1dc71c7df448f3a14b75b8a131fe3
takeharu73 <takeharu730@gmail.com>
2021-10-26 22:07:05 +0900

    add abs()

--- 20211026/JOI 2009 本選 2 - ピザ.py
+++ 20211026/JOI 2009 本選 2 - ピザ.py
@@ -13,8 +13,9 @@ import bisect
 res = 0
 for k in K:
     index = bisect.bisect_left(D, k)
-    if D[index]-k>k-D[index-1]:
-        res+=(k-D[index-1])
+    # absを入れないとWAになる
+    if D[index]-k>abs(k-D[index-1]):
+        res+=(abs(k-D[index-1]))
     else:
         res+=(D[index]-k)
     
